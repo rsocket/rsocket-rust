fbec90dbd3a3c2d87dd670efa55f879e69b0e7c3
Jeffsky <jjeffcaii@outlook.com>
2020-12-13 23:11:06 +0800

    chore: prelease 0.6

--- rsocket-benchmark/benchmark_main.rs
+++
@@ -1,8 +0,0 @@
-use criterion::criterion_main;
-
-mod benchmarks;
-
-criterion_main! {
-    benchmarks::fibonaccis::benches,
-    benchmarks::frames::benches,
-}
--- rsocket-benchmark/benchmarks/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod fibonaccis;
-pub mod frames;
