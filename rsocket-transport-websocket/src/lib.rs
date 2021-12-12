20b0adae8310ee06e0c26ece8be92d88fb62b112
Jeffsky <jjeffcaii@outlook.com>
2021-12-12 22:37:45 +0800

    feat: support custom websocket request (#52)

--- rsocket-transport-websocket/src/lib.rs
+++ rsocket-transport-websocket/src/lib.rs
@@ -7,5 +7,35 @@ mod client;
 mod connection;
 mod server;
 
-pub use client::WebsocketClientTransport;
+pub use client::{WebsocketClientTransport, WebsocketRequest};
 pub use server::WebsocketServerTransport;
+
+#[cfg(test)]
+mod test_websocket {
+    use super::*;
+    use rsocket_rust::prelude::*;
+
+    #[ignore]
+    #[tokio::test]
+    async fn test_client() {
+        let req: WebsocketRequest = WebsocketRequest::builder()
+            .uri("ws://127.0.0.1:8080/hello")
+            .header("x-foo-bar", "42")
+            .method("GET")
+            .body(())
+            .unwrap();
+        let tp = WebsocketClientTransport::from(req);
+        let c = RSocketFactory::connect()
+            .transport(tp)
+            .start()
+            .await
+            .expect("connect failed");
+        let res = c
+            .request_response(Payload::builder().set_data_utf8("foo").build())
+            .await
+            .expect("request failed")
+            .unwrap();
+
+        println!("response: {:?}", res);
+    }
+}
